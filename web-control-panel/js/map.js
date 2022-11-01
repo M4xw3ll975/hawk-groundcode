1875551bdf252b38ea1412889f536976a753d115
Maximilian <66524685+M4xw3ll975@users.noreply.github.com>
2022-11-01 21:12:29 +0100

    added file upload dialog and placeholder text

--- web-control-panel/js/map.js
+++ web-control-panel/js/map.js
@@ -2,8 +2,10 @@
 
 'use strict';
 
-//Mapbox map
+//html elements
+const loadButton = document.getElementById('load');
 
+//Mapbox map
 //access token
 mapboxgl.accessToken = 'pk.eyJ1IjoibTR4dzNsbCIsImEiOiJjbDl4NzhsbGMwNzY3M3Fub3JhbnZldGhnIn0.9Y1af9ijoJiMuKxBe0CZyw';
 
@@ -16,3 +18,53 @@ var map = new mapboxgl.Map({
     center: [-87.661557, 41.893748],
     zoom: 10.7
 });
+
+//add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
+
+
+//when load button is clicked open a promt file input dialog that allows the user to select a gpx file
+loadButton.onclick = function() {
+    let fileInput = document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept = '.gpx';
+    fileInput.onchange = function() {
+        //get the file
+        let file = fileInput.files[0];
+
+        //create a file reader
+        let reader = new FileReader();
+
+        //when the file is loaded
+        reader.onload = function() {
+            //get the file contents
+            let fileContents = reader.result;
+
+            //parse the gpx file
+            let gpx = toGeoJSON.gpx(new DOMParser().parseFromString(fileContents, 'text/xml'));
+
+            //add the gpx file to the map
+            map.addSource('gpx', {
+                type: 'geojson',
+                data: gpx
+            });
+
+            //add the gpx file to the map as a layer
+            map.addLayer({
+                id: 'gpx',
+                type: 'line',
+                source: 'gpx',
+                paint: {
+                    'line-color': '#ff0000',
+                    'line-width': 3
+                }
+            });
+        };
+
+        //read the file
+        reader.readAsText(file);
+    };
+    fileInput.click();
+    
+};
